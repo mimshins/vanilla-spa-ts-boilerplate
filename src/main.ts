ee64a29a82c6edc8e943b02c39a57a6be97ba4cc
mimshins <mostafa.sh.coderino@gmail.com>
2022-08-29 16:06:49 +0430

    refactor due to api changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { InternalError, NotFoundError } from "errors";
-import { createBrowserHistory, type Location } from "history";
+import configureRoutes from "configureRoutes";
+import { createBrowserHistory } from "history";
+import "normalize.css";
 import {
   ExampleDynamicPage,
   HomePage,
   InternalErrorPage,
   NotFoundPage
 } from "pages";
-import { createDynamicRoute, handleDynamicRoutes } from "utils";
-import "normalize.css";
 import "./style.css";
 
 const appRootElement = <HTMLDivElement | null>document.getElementById("app");
@@ -17,49 +16,11 @@ if (!appRootElement)
 
 const history = createBrowserHistory();
 
-const switchRoutes = (location: Location) => {
-  try {
-    switch (location.pathname) {
-      case "/":
-        return HomePage({ appRootElement, history, pageProps: {} });
-      default: {
-        // In default we match dynamic routes
-        const matched = handleDynamicRoutes(location, [
-          createDynamicRoute("/user/:id", ExampleDynamicPage, {
-            appRootElement,
-            history,
-            pageProps: {}
-          })
-        ]);
-
-        if (!matched) throw new NotFoundError();
-      }
-    }
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      return NotFoundPage({
-        appRootElement,
-        history,
-        pageProps: { message: err.message }
-      });
-    } else if (err instanceof InternalError) {
-      return InternalErrorPage({
-        appRootElement,
-        history,
-        pageProps: { message: err.message }
-      });
-    }
-
-    return InternalErrorPage({
-      appRootElement,
-      history,
-      pageProps: {}
-    });
-  }
-};
-
-// Initial routing
-switchRoutes(history.location);
-
-// Listen for route changes
-history.listen(({ location }) => switchRoutes(location));
+configureRoutes(appRootElement, history, {
+  errorPage: InternalErrorPage,
+  notFoundPage: NotFoundPage,
+  routes: [
+    { path: "/", page: HomePage },
+    { path: "/user/:id", page: ExampleDynamicPage }
+  ]
+});
